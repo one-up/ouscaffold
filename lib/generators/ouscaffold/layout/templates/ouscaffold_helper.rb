d3ff312ad63be465a552b0aafee3a0cc40fd4392
holysugar <holy@oneup-inc.com>
2010-09-28 10:29:42 +0900

    make zebra-striped table

--- lib/generators/ouscaffold/layout/templates/ouscaffold_helper.rb
+++ lib/generators/ouscaffold/layout/templates/ouscaffold_helper.rb
@@ -2,4 +2,15 @@ module OuscaffoldHelper
   def title
     @title ? @title : action_name
   end
+
+  def tr_with_zebra(options = {})
+    options = options.dup
+    t = cycle('odd', 'even')
+    if options[:class]
+      options[:class] << " #{t}"
+    else
+      options[:class] = t
+    end
+    tag("tr", options, true)
+  end
 end
