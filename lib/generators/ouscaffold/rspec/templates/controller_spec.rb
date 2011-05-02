271be8d39553a13fa80703c0ab6e9b1c357532c6
mewlist(Hidenori Doi) <hdoi@oneup-inc.com>
2011-05-02 18:16:59 +0900

    Fix for rspec-rails 1.5 (stub!,should_receive! => stub,should_receive)

--- lib/generators/ouscaffold/rspec/templates/controller_spec.rb
+++ lib/generators/ouscaffold/rspec/templates/controller_spec.rb
@@ -9,7 +9,7 @@ describe <%= controller_class_name %>Controller do
 <% unless options[:singleton] -%>
   describe "GET index" do
     it "assigns all <%= plural_table_name %> as @<%= plural_table_name %>" do
-      <%= stub! orm_class.all(class_name) %> { [<%= mock_file_name %>] }
+      <%= stub orm_class.all(class_name) %> { [<%= mock_file_name %>] }
       get :index
       assigns(:<%= plural_table_name %>).should eq([<%= mock_file_name %>])
     end
@@ -18,7 +18,7 @@ describe <%= controller_class_name %>Controller do
 <% end -%>
   describe "GET show" do
     it "assigns the requested <%= singular_table_name %> as @<%= singular_table_name %>" do
-      <%= stub! orm_class.find(class_name, "37".inspect) %> { <%= mock_file_name %> }
+      <%= stub orm_class.find(class_name, "37".inspect) %> { <%= mock_file_name %> }
       get :show, :id => "37"
       assigns(:<%= singular_table_name %>).should be(<%= mock_file_name %>)
     end
@@ -26,7 +26,7 @@ describe <%= controller_class_name %>Controller do
 
   describe "GET new" do
     it "assigns a new <%= singular_table_name %> as @<%= singular_table_name %>" do
-      <%= stub! orm_class.build(class_name) %> { <%= mock_file_name %> }
+      <%= stub orm_class.build(class_name) %> { <%= mock_file_name %> }
       get :new
       assigns(:<%= singular_table_name %>).should be(<%= mock_file_name %>)
     end
@@ -34,7 +34,7 @@ describe <%= controller_class_name %>Controller do
 
   describe "GET edit" do
     it "assigns the requested <%= singular_table_name %> as @<%= singular_table_name %>" do
-      <%= stub! orm_class.find(class_name, "37".inspect) %> { <%= mock_file_name %> }
+      <%= stub orm_class.find(class_name, "37".inspect) %> { <%= mock_file_name %> }
       get :edit, :id => "37"
       assigns(:<%= singular_table_name %>).should be(<%= mock_file_name %>)
     end
@@ -44,13 +44,13 @@ describe <%= controller_class_name %>Controller do
 
     describe "with valid params" do
       it "assigns a newly created <%= singular_table_name %> as @<%= singular_table_name %>" do
-        <%= stub! orm_class.build(class_name, params) %> { <%= mock_file_name(:save => true) %> }
+        <%= stub orm_class.build(class_name, params) %> { <%= mock_file_name(:save => true) %> }
         post :create, :<%= singular_table_name %> => <%= params %>
         assigns(:<%= singular_table_name %>).should be(<%= mock_file_name %>)
       end
 
       it "redirects to the created <%= singular_table_name %>" do
-        <%= stub! orm_class.build(class_name) %> { <%= mock_file_name(:save => true) %> }
+        <%= stub orm_class.build(class_name) %> { <%= mock_file_name(:save => true) %> }
         post :create, :<%= singular_table_name %> => {}
         response.should redirect_to(<%= singular_table_name %>_url(<%= mock_file_name %>))
       end
@@ -58,13 +58,13 @@ describe <%= controller_class_name %>Controller do
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved <%= singular_table_name %> as @<%= singular_table_name %>" do
-        <%= stub! orm_class.build(class_name, params) %> { <%= mock_file_name(:save => false) %> }
+        <%= stub orm_class.build(class_name, params) %> { <%= mock_file_name(:save => false) %> }
         post :create, :<%= singular_table_name %> => <%= params %>
         assigns(:<%= singular_table_name %>).should be(<%= mock_file_name %>)
       end
 
       it "re-renders the 'new' template" do
-        <%= stub! orm_class.build(class_name) %> { <%= mock_file_name(:save => false) %> }
+        <%= stub orm_class.build(class_name) %> { <%= mock_file_name(:save => false) %> }
         post :create, :<%= singular_table_name %> => {}
         response.should render_template("new")
       end
@@ -76,19 +76,19 @@ describe <%= controller_class_name %>Controller do
 
     describe "with valid params" do
       it "updates the requested <%= singular_table_name %>" do
-        <%= should_receive! orm_class.find(class_name, "37".inspect) %> { <%= mock_file_name %> }
-        mock_<%= should_receive! orm_instance.update_attributes(params) %>
+        <%= should_receive orm_class.find(class_name, "37".inspect) %> { <%= mock_file_name %> }
+        mock_<%= should_receive orm_instance.update_attributes(params) %>
         put :update, :id => "37", :<%= singular_table_name %> => <%= params %>
       end
 
       it "assigns the requested <%= singular_table_name %> as @<%= singular_table_name %>" do
-        <%= stub! orm_class.find(class_name) %> { <%= mock_file_name(:update_attributes => true) %> }
+        <%= stub orm_class.find(class_name) %> { <%= mock_file_name(:update_attributes => true) %> }
         put :update, :id => "1"
         assigns(:<%= singular_table_name %>).should be(<%= mock_file_name %>)
       end
 
       it "redirects to the <%= file_path %>" do
-        <%= stub! orm_class.find(class_name) %> { <%= mock_file_name(:update_attributes => true) %> }
+        <%= stub orm_class.find(class_name) %> { <%= mock_file_name(:update_attributes => true) %> }
         put :update, :id => "1"
         response.should redirect_to(<%= singular_table_name %>_url(<%= mock_file_name %>))
       end
@@ -96,13 +96,13 @@ describe <%= controller_class_name %>Controller do
 
     describe "with invalid params" do
       it "assigns the <%= singular_table_name %> as @<%= singular_table_name %>" do
-        <%= stub! orm_class.find(class_name) %> { <%= mock_file_name(:update_attributes => false) %> }
+        <%= stub orm_class.find(class_name) %> { <%= mock_file_name(:update_attributes => false) %> }
         put :update, :id => "1"
         assigns(:<%= singular_table_name %>).should be(<%= mock_file_name %>)
       end
 
       it "re-renders the 'edit' template" do
-        <%= stub! orm_class.find(class_name) %> { <%= mock_file_name(:update_attributes => false) %> }
+        <%= stub orm_class.find(class_name) %> { <%= mock_file_name(:update_attributes => false) %> }
         put :update, :id => "1"
         response.should render_template("edit")
       end
@@ -112,13 +112,13 @@ describe <%= controller_class_name %>Controller do
 
   describe "DELETE destroy" do
     it "destroys the requested <%= singular_table_name %>" do
-      <%= should_receive! orm_class.find(class_name, "37".inspect) %> { <%= mock_file_name %> }
-      mock_<%= should_receive! orm_instance.destroy %>
+      <%= should_receive orm_class.find(class_name, "37".inspect) %> { <%= mock_file_name %> }
+      mock_<%= should_receive orm_instance.destroy %>
       delete :destroy, :id => "37"
     end
 
     it "redirects to the <%= singular_table_name %> list" do
-      <%= stub! orm_class.find(class_name) %> { <%= mock_file_name %> }
+      <%= stub orm_class.find(class_name) %> { <%= mock_file_name %> }
       delete :destroy, :id => "1"
       response.should redirect_to(<%= plural_table_name %>_url)
     end
